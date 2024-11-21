408a7c3c1a41ade030aa1db8a05722edf6763328
AlanaMarg <alana.pereiraa61@gmail.com>
2024-11-21 07:03:09 -0300

    vetor

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main()
+{
+    int *vetor, size;
+    scanf("%d", &size);
+    
+    if(size == 0) {
+        printf("[vazio]");
+    }
+    
+    vetor = (int *) malloc(size * sizeof(int));
+    
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &vetor[i]);
+    }
+    
+    for(int i = 0; i < size; i++) {
+        if(i == 0) {
+            printf("[");
+        }
+        
+        printf("%d", vetor[i]);
+        
+        if(i < size - 1) {
+            printf(",");
+        } else {
+            printf("]");
+        }
+    }
 
     return 0;
 }
